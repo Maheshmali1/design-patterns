c8053850f34cb0be7908da6dd1aa186065e599e1
mahesh mali <mahimali15082001@gmail.com>
2023-12-02 22:36:36 +0530

    chore: cz config addition

---
+++ .cz-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    types: [
+      { value: 'feat', name: 'feat: A new feature' },
+      { value: 'fix', name: 'fix: A bug fix' },
+      { value: 'docs', name: 'docs: Documentation only changes' },
+      { value: 'chore', name: 'chore: Changes to build process' },
+    ],
+    scopes: [
+      { value: '', name: 'All scopes' },
+      { value: 'abstract-factory', name: 'abstract-factory pattern' },
+    ],
+  };
+  

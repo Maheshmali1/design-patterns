97fe4bb2b03b551f3a40995caed6200c33652d3c
mahesh mali <mahimali15082001@gmail.com>
2023-12-04 10:33:01 +0530

    chore(singleton): pr comment resolution

--- src/singleton-pattern/client.ts
+++ src/singleton-pattern/client.ts
@@ -2,17 +2,17 @@ import { Game1, Game2, Game3 } from './singleton-users';
 
 const game1 = new Game1();
 game1.addWinner(1, 'ABC');
-console.log(`Printing scoreboard aftering adding winner from game1`);
+console.log(`Printing scoreboard after adding winner from game1`);
 game1.leaderboard.printLeaderboard();
 console.log();
 
 const game2 = new Game2();
 game2.addWinner(2, 'DEF');
-console.log(`Printing scoreboard aftering adding winner from game2`);
+console.log(`Printing scoreboard after adding winner from game2`);
 game2.leaderboard.printLeaderboard();
 console.log();
 
 const game3 = new Game3();
 game3.addWinner(3, 'GHI');
-console.log(`Printing scoreboard aftering adding winner from game3`);
+console.log(`Printing scoreboard after adding winner from game3`);
 game3.leaderboard.printLeaderboard();
